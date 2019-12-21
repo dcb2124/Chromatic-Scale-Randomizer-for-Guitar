389a7ec46e798d03d6635ed396be9c09238a4a2a
David Billingsley <dcb2124@gmail.com>
2019-12-21 04:32:44 +0000

    It now makes random choice of sharp or flat as well

--- main.py
+++ main.py
@@ -8,8 +8,35 @@ string = strings.split()
 sharps = chromaticSharp.split()
 flats = chromaticFlat.split()
 
+print("sharps and flats separate")
 for x in range(6):
   random.shuffle(sharps)
   random.shuffle(flats)
   print(string[x] + " string sharps: " + str(sharps))
-  print(string[x] + " string flats: " + str(flats))
+  print(string[x] + " string flats:  " + str(flats))
+
+print("")
+print("random choice of sharps and flats")
+
+
+notes = [
+  ["C"],
+  ["C#", "D♭"],
+  ["D"],
+  ["E"],
+  ["F"],
+  ["G"],
+  ["A"],
+  ["B"],
+  ["D♯"],
+  ["F♯", "Gb"],
+  ["G♯","A♭"],
+  ["A♯","B♭"], 
+]
+
+for x in range(6):
+  random.shuffle(notes)
+  melody = []
+  for y in range(len(notes)):
+    melody.append(random.choice(notes[y]))
+  print(string[x] + " string: " + str(melody))
